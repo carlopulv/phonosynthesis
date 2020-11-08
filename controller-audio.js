5542b972acaa3ef605e2b641a47700f5afe62ccb
GabrieleMarelli <g.marelli96@hotmail.it>
2020-11-08 17:41:11 +0100

    midi1

--- controller-audio.js
+++ controller-audio.js
@@ -67,3 +67,44 @@ document.body.onkeyup = function(e) {
     gains[e.key].gain.linearRampToValueAtTime(0, c.currentTime + 0.1)*/
     notes.pop();
 }
+
+
+
+
+
+//take midi input PROVA
+
+var midi, data;
+// request MIDI access
+if (navigator.requestMIDIAccess) {
+    navigator.requestMIDIAccess({
+        sysex: false
+    }).then(onMIDISuccess, onMIDIFailure);
+} else {
+    alert("No MIDI support in your browser");
+}
+
+// midi functions
+function onMIDISuccess(midiAccess) {
+    console.log('MIDI Access Object', midiAccess);
+    midi = midiAccess; // this is our raw MIDI data, inputs, outputs, and sysex status
+
+    var inputs = midi.inputs.values();
+    // loop over all available inputs and listen for any MIDI input
+    for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
+        input.value.onmidimessage = onMIDIMessage;
+    }
+}
+
+function onMIDIFailure(error) {
+    console.log("No access to MIDI devices or your browser doesn't support WebMIDI API. Please use WebMIDIAPIShim " + error);
+}
+
+function onMIDIMessage(message) {
+    data = message.data; // this gives us our [command/channel, note, velocity] data.
+    console.log('MIDI data',data); 
+
+}
+
+
+
